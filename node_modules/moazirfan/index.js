4560307e7d05311dc33a9bd19dd95607a8c69d60
MoazIrfan <irfanmoaz@gmail.com>
2023-10-13 19:41:46 +0500

    change package name

---
+++ node_modules/moazirfan/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+console.log(`
+Moaz Irfan
+
+I love creating automation tools to speed up development.
+
+Twitter: https://twitter.com/MoazIrfan
+Github:  htpps://github.com/MoazIrfan
+Blog:    https://moazirfan.com
+
+`);
