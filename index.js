96cecae1d5d5576630083ad19f4ada6acd5ca061
MoazIrfan <irfanmoaz@gmail.com>
2023-10-17 10:58:23 +0500

    updated bio info

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 console.log(`
 Moaz Irfan
 
-I love creating automation tools to speed up development.
+I love creating automation tools to speed up development... Check out my [personal website](https://moazirfan.com) to discover some cool stuff about me.
 
 Twitter: https://twitter.com/MoazIrfan
-Github:  htpps://github.com/MoazIrfan
+Github:  https://github.com/MoazIrfan
 Blog:    https://moazirfan.com
 
 `);
